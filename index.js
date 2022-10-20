05aafa3d8f6416ee54f59911ecc47a9a76716a3e
space-waffle <115738661+space-waffle@users.noreply.github.com>
2022-10-20 18:57:07 +0000

    implement simple server

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-var num = 5;
-console.log({num});
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, world')
+})
+
+app.listen(3030, () => {
+    console.log('simple-server running on port 3030');
+})
